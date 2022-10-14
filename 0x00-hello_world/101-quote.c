a6861d978184b9e00195d6bc712fe04590ac692c
roswaititu <rosewaitera2014@gmail.com>
2022-10-14 10:26:52 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+      */
 int main(void)
 {
-fprintf(stderr,"and that piece f art is useful\"- Dora Korpar, 201-10-19\n");
-    return (1);
+fprintf(stderr, "and that piece f art is useful\"- Dora Korpar, 201-10-19\n");
+return (1);
 }
