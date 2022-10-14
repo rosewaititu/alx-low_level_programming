d9fe5037d05438f1b15cbe3115aae0e64748c86d
roswaititu <rosewaitera2014@gmail.com>
2022-10-14 06:39:06 -0700

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *    * main - print to screen
+ *       *
+ *          * Return: (0)
+  */
+int main(void)
+{
+		char Mystr[] = "\"with proper grammar, but the outcome is a piece of art,";
+
+			printf(Mystr);
+				return (0);
+}
