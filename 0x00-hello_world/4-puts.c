c60d15cfd46cb2db1c87d7f6961418dd6389602b
roswaititu <rosewaitera2014@gmail.com>
2022-10-14 06:34:10 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *    * main - print to screen
  *       *
  *          * Return: zero
- *             */
+  */
 int main(void)
 {
 		char Mystr[] = "\"Programming is like building a multilingual puzzle";
