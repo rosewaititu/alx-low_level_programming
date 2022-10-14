4a49b773b134bd0ef78c33f43b0210810741b789
roswaititu <rosewaitera2014@gmail.com>
2022-10-14 06:49:05 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *    * main - print to screen
+ *    * main - Printng a statement on the screen
  *       *
- *          * Return: (0)
+ *          * Return: zero
   */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n):";
-
-				return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
