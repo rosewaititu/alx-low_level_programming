36cd1a3a95edb53f1050e2eaaedc1e4cb3dffe82
roswaititu <rosewaitera2014@gmail.com>
2022-10-15 14:17:52 -0700

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints base ten digits without char
+ *
+ * *
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+putchar(a);
+putchar('\n')
+return (0);
+}
