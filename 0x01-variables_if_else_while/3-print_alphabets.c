a68189953e1f8c359e0d77fcdc0b5f44be2b979a
roswaititu <rosewaitera2014@gmail.com>
2022-10-15 12:05:34 -0700

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-prints upper and lowr alphabet
+ *
+ * return:always zero
+ */
+int main(void)
+{
+char c = 'a';
+char C = 'A';
+for (c='a'; c <= 'z'; c++)
+putchr(c);
+for (C ='A';C <= 'Z' C++)
+putchr(C);
+putchr('\n');
+return (0);
+}
