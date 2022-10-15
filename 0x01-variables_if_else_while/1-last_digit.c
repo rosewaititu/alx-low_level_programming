5b53c255c72844368fa6f5599aa83f96cf66a9ff
roswaititu <rosewaitera2014@gmail.com>
2022-10-15 08:34:03 -0700

    task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,61 @@
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- * main-prints the last digit and compare it
- *
- *
- * Return:always zero
- */
-int main(void)
-{
-int n;
-int a;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-a = n % 10;
-if (a > 5)
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *    * main - Prints last digit
+ *
+ *       *
+ *
+ *          *Return: Always zero
+ *
+ *             */
+
+int main(void)
+
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, a);
+
+		int n;
+
+			int a;
+
+
+
+				srand(time(0));
+
+					n = rand() - RAND_MAX / 2;
+
+						a = n % 10;
+
+
+
+							if (a > 5)
+
+									{
+
+												printf("Last digit of %d is %d and is greater than 5\n", n, l);
+
+													} else if (a == 0)
+
+															{
+
+																		printf("Last digit of %d is %d and is 0\n", n, a);
+
+																			} else if (a < 6 && l != 0)
+
+																					{
+
+																								printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+
+																									}
+
+																				return (0);
+
 }
-else if (n == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, a);
-}
-else if(a > 6 && a!= 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
-}
-return (0);
-}
+
+
