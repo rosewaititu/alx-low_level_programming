3fa4f9476c265dcb464b08b5e43a1ebbd9462db0
roswaititu <rosewaitera2014@gmail.com>
2022-10-15 13:29:57 -0700

    task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,15 +4,15 @@
 
 /**
   * main - print digits of base ten
- *
-*     *
-          * Return: zero
+  *
+  *
+  * Return: zero
     */
 
 int main(void)
 {
 int i;
-for (i = 0;i <= 9; i++)
+for (i = 0; i <= 9; i++)
 printf("%d", i);
 printf("\n");
 return (0);
