6a9f6dd140b692efb73b83261b5af15ccbd917f7
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 02:11:02 -0700

    print hexadecimal in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-prints hexadecimal in lower alphabet
+ *
+ * Return:Always zero
+ */
+int main(void)
+{
+int i;
+char c;
+for (i = 0; i <= 9; i++)
+putchar(i);
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return(0)
+}
