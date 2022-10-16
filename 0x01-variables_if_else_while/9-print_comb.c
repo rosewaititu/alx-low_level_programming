0cd9bc5e23444dc19d0f256c98e3bae881ccc231
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 05:38:22 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,13 @@
 int main(void)
 {
 		int d;
-
-			for (d = 0; d <= 9; d++)
-					{
-								putchar((d % 10) + '0');
-										if (d == 9)
-														continue;
-
-												putchar(',');
-														putchar(' ');
-															}
-
-				putchar('\n');
-					return (0);
+for (d = 0; d <= 9; d++)
+putchar((d % 10) + '0');
+if (d == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
