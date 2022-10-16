2b4b7fb76e6faea5ea2ff6f20b4d85f3fc62ebbb
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 02:42:29 -0700

    print hexadecimal in lower case

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -pront numbers with comma and space
+ *
+ *
+ * Return:Always zero
+ */
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+if (x == 9)
+break;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
