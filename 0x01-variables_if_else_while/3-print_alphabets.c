c66770401ba8990c7ba7207dd0a5405ca2c75331
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 03:52:44 -0700

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 char c = 'a';
 char C = 'A';
-for (c='a'; c <= 'z'; c++)
+for (c='a'; c <=; 'z'; c++)
 putchar(c);
-for (C ='A';C <= 'Z' C++)
+for (C ='A';C <= 'Z'; C++)
 putchar(C);
 putchar('\n');
 return (0);
