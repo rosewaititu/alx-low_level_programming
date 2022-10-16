714828e7f5bf040045ebec6f5a5af2ddf61f6882
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 07:41:01 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
-   * Description: main - -prints out a string stdio
-    *
-     * Return: 1 if successful
-     */
-
+     * main - prints to the standard error
+        *
+           * Return: one
+              */
 int main(void)
-
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+		fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 			return (1);
 }
