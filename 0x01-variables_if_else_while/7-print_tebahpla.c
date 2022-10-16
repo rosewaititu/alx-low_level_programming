f19eaf34d552c6bcd031db7a5392f35db94b09ea
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 00:44:53 -0700

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-prints alphabets in reverse
+ *
+ * return:Always zero
+ */
+int main (void);
+{
+char x;
+for(x = 'z'; x <= 'a'; x--)
+putchar(x);
+putchar('/n');
+return(0)
+}
