58b183082c8c094db794bc23160fb81ba3a483f8
roswaititu <rosewaitera2014@gmail.com>
2022-10-16 05:52:04 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
  *  * Description: main - prints numbers
- *   *
- *    * Return: 0 is successful
- *    */
+ *
+ *
+ *  Return: 0 is successful
+  */
 
 int main(void)
 {
 		int d;
 
-			for (d = 0; d <= 9; d++)
+for (d = 0; d <= 9; d++)
 					{
 								putchar((d % 10) + '0');
-										if (d == 9)
+if (d == 9)
 														continue;
 
 												putchar(',');
