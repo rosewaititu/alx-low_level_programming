2facaf074f09baa6c5ad1bbf543d0e209924406d
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 04:01:10 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main:prints alpabets
+ *
+ * Return:always zero
+ */void print_alphabet(void)
+{
+	char x ='a'; 
+	for (x='a'; x <= 'z'; x++)
+		 
+		 putchar(x);
+	putchar('\n');
+}		
