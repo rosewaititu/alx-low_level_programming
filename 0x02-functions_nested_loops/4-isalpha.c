860fe1943a553bb0394e51377c0c43e2265fcbc9
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 05:36:58 -0700

    task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+   * main - check the code.
+    *
+     * Return: Always 0.
+      */
+int int _isalpha(int c);
+{
+	{
+			return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	}
