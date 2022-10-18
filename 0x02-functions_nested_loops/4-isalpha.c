39cf4cd81e1f68d606dfde40dd4e732f57f06dcf
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 07:15:15 -0700

    task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,9 @@
 #include "main.h"
  /**
-Desctription _iaalpha -prints lphabet
-
-@c output
-Return:always zero
-
-
+* Desctription _isalpha -prints lphabet
+*
+*@c output
+*Return:always zero
 */
 int _isalpha(int c)
 
