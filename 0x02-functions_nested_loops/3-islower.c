476a257ff20d33e02c1f3328fb894f52cec2b790
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 08:02:05 -0700

    task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
+#include "main.h"
 /**
-   *_islower - check the code.
-    *@c -character to be ccked
-     * Return: Always 0.
+ * _islower - controls if a character is in lowercase
+ * @c: character to be verified
+ * Return: return 0 or 1
  */
 int _islower(int c)
 {
