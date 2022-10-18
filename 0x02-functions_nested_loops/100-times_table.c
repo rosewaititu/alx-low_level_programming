b01b1f23b719d7af28215e30d41a7eac0efb9241
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 11:27:53 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,9 @@ void print_times_table(int n)
 {
 int digit, mult, result;
 if (n <= 15 && n >= 0)
-{
 for (digit = 0; digit <= n; digit++)
 {
-_putchar('0');
+	putchar('0');
 for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
