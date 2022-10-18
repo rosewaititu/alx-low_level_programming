b69a5db711349588b6438a9ef7ceb85e9b022c3e
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 11:58:17 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,33 +5,52 @@
  * Return: returns nothing
  */
 void print_times_table(int n)
+
+ {
+int a, b, c;
+if (n >= 0 && n <= 15)
 {
-int digit, mult, result;
-if (n <= 15 && n >= 0)
-for (digit = 0; digit <= n; digit++)
+for (a = 0; a <= n; a++)
 {
-	putchar('0');
-for (mult = 1; mult <= n; mult++)
+_putchar('0');
+for (b = 1; b <= n; b++)
 {
 _putchar(',');
 _putchar(' ');
-result = digit * mult;
-if (result <= 99)
-_putchar(' ');
-if (result <= 9)
-_putchar(' ');
-if (result >= 100)
+c = a * b;
+if (c <= 99)
 {
-_putchar((result / 100) + '0');
-_putchar((result / 10) % 10 + '0');
-}
-else if (result <= 99 && result >= 10)
+_putchar(' ')
+}if (c <= 9)
 {
-_putchar((result / 10) + '0');
+_putchar(' ');
 }
-_putchar((result % 10) + '0');
-}
-_putchar('\n');
-}
-}
+if (c >= 100)
+{
+_putchar((c / 100) + '0');
+
+																																																																																_putchar(((c / 10)) % 10 + '0');
+
+																																																																																				}
+
+
+
+																																																																																								else if (c <= 99 && c >= 10)
+
+																																																																																												{
+
+					_putchar((c / 10) + '0');
+
+									}
+
+				_putchar((c % 10) + '0');
+
+							}
+
+			_putchar('\n');
+
+					}
+
+	}
+
 }
