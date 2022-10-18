e0e2bb4772ccdf110634f06a4d83378b952c8d64
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 06:33:26 -0700

    task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 		int hour;
 			int minute;
-
-				for (hour = 0; hour <= 23; hour++)
-						for (minute = 0; minute <= 59; minute++)
-								{
-											_putchar('0' + hour / 10);
-													_putchar('0' + hour % 10);
-															_putchar(':');
-																	_putchar('0' + minute / 10);
-																			_putchar('0' + minute % 10);
-																					_putchar('\n');
-																						}
+for (hour = 0; hour <= 23; hour++)
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar('0' + hour / 10);
+_putchar('0' + hour % 10);
+_putchar(':');
+_putchar('0' + minute / 10);
+_putchar('0' + minute % 10);
+_putchar('\n');
+}
+																			
 }
