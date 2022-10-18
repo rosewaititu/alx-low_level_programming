9f14b6f37355e6e69dde4a02371b2987d45b052d
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 05:31:14 -0700

    task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+   * main - check the code.
+    *
+     * Return: Always 0.
+ */
+int _islower(int c)
+ {
+ 	if (c >= 97 && c <= 122)
+		{
+				return (1);
+					}
+						else
+							{
+									return (0);
+										}
+										}
