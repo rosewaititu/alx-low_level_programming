858f75611aa21ca1a753da34889d03db0e5f486a
roswaititu <rosewaitera2014@gmail.com>
2022-10-18 12:10:15 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,29 +29,14 @@ _putchar(' ');
 if (c >= 100)
 {
 _putchar((c / 100) + '0');
-
-																																																																																_putchar(((c / 10)) % 10 + '0');
-
-																																																																																				}
-
-
-
-																																																																																								else if (c <= 99 && c >= 10)
-
-																																																																																												{
-
-					_putchar((c / 10) + '0');
-
-									}
-
-				_putchar((c % 10) + '0');
-
-							}
-
-			_putchar('\n');
-
-					}
-
-	}
-
+_putchar(((c / 10)) % 10 + '0');
+else if (c <= 99 && c >= 10)
+{
+_putchar((c / 10) + '0');
+}
+_putchar((c % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
