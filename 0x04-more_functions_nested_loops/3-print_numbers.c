e6c8c9270e0e690860bb72dd0c5dd5036cf630d8
roswaititu <rosewaitera2014@gmail.com>
2022-10-21 07:50:03 -0700

    more functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 void print_numbers(void)
 {
 	int number = 48;
-	for (number = 48; number < 58; number++)
-	{
-	_putchar(number);
-	}
-	_putchar(10);
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
