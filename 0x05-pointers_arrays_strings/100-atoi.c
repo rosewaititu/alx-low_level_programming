827d926a7dceaf3b9b0cb57221914b24676ad232
roswaititu <rosewaitera2014@gmail.com>
2022-10-25 06:19:47 -0700

    task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,22 +5,17 @@
  * @s: string
  * Return: The integer converted
   */
-
 int _atoi(char *s)
 {
-int i, j = 0, k = -1, l = 0;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == '-')
-k = k * -1;															}
-if (s[i] >= '0' && s[i] <= '9')
-{
-j = j * 10;
-j -= (s[i] - '0');
-l = 1;
-}
-else if (l == 1)
-break;
-j = k * j;i					
-return (j);
+	int i = 1;
+	unsigned int num = 0;
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num *i);
 }
