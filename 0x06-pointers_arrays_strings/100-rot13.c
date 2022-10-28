019973cda4797452f58fc1b0facace60c210cbcc
roswaititu <rosewaitera2014@gmail.com>
2022-10-28 14:15:08 -0700

    task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *rot13 - encodes in rot13*   
+ * @s: string
+ * Return: Always successful
+ */
+
+char *rot13(char *s)
+{
+int i, j;
+char inp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (i = 0; s[i] != '\0'; i++)
+for (j = 0; j < 54; j++)
+{
+if (s[i] == inp[j])
+{
+s[i] = rot[j];
+break;															}
+}
+}
+return (s);
+}
