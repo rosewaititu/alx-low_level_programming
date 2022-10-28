b95a3bc75a94a75e1aee25534093cddff17f4a7b
roswaititu <rosewaitera2014@gmail.com>
2022-10-28 13:02:50 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,8 +5,7 @@
  *the end of the string pointed to by @dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
- * 
- * Return: returns poiner to @des
+ *Return: returns poiner to @des
   */
 
 char *_strcat(char *dest, char *src)
