cc39379ab676cbe8b8669625c667008033988a85
roswaititu <rosewaitera2014@gmail.com>
2022-10-28 19:07:02 -0700

    task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,8 +2,9 @@ nclude "main.h"
 /**
  *print_number - prints an integer
  *@n:integer to be printed
+ *
  */
-void print_number(int n)
+ void print_number(int n)
 {
 unsigned int num = n;
 if (n < 0)
