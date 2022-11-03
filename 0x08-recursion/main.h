37c120870ec0c31ce8968e113eb9b9ceee20cb9f
roswaititu <rosewaitera2014@gmail.com>
2022-11-03 08:39:55 -0700

    task

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int square(int n, int sqr);
+int is_prime_number(int n);
+int checks_prime(int n, int b);
+int is_palindrome(char *s);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
