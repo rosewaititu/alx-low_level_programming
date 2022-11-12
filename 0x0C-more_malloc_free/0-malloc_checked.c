8e74a674b614851a4fe8453ba3255963fddc448d
roswaititu <rosewaitera2014@gmail.com>
2022-11-12 06:29:17 -0800

    task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,43 +2,17 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates 2 strings
- * @s1: string 1
- * @s2: string 2
+ * malloc_checked - allocates memory
  *
- * Return: Null if unsuccessful
+ * @b: integer
+ * Return: pointer
  */
 
-char *str_concat(char *s1, char *s2)
+void *malloc_checked(unsigned int b)
 {
-	char *s;
-	unsigned int i, j, k, l;
+	void *chk = malloc(b);
 
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-
-	s = malloc(sizeof(char) * (i + j + 1));
-
-	if (!s)
-	{
-		free(s);
-		return (NULL);
-	}
-
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-
-	l = j;
-
-	for (j = 0; j <= l; k++, j++)
-		s[k] = s2[j];
-
-	return (s);
+	if (chk == NULL)
+		exit(98);
+	return (chk);
 }
