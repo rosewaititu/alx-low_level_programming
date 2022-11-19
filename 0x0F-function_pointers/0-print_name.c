df5687a17c3d49be4af039d0686059a3e855408d
roswaititu <rosewaitera2014@gmail.com>
2022-11-19 01:12:27 -0800

    task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
-*print_name - prints a name
-*@name:name to print
-*@f: function which print name
-*/
-
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to function f.
+ * @f
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-
-if (name == NULL || f == NULL)
-return;
-
-f(name);
-
+	if (f)
+		f(name);
 }
